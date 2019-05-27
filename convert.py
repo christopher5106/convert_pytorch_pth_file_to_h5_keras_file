4f2daf102186e6973a0d0e318efe3ec953937f06
chris <cbo@ivalua.com>
2019-05-27 14:58:30 +0200

    n

--- convert.py
+++ convert.py
@@ -1,3 +1,22 @@
 import torch
 filename = '/sharedfiles/XLM/mlm_tlm_xnli15_1024.pth'
-torch_model = torch.load(filename)
+reloaded = torch.load(filename)
+
+print(reloaded)
+
+# ['model_weights', 'optimizer_weights']
+# list(f1["model_weights"])
+#
+# #!/usr/bin/env python
+# import h5py
+#
+# # Create random data
+# import numpy as np
+# data_matrix = np.random.uniform(-1, 1, size=(10, 3))
+#
+# # Write data to HDF5
+# data_file = h5py.File('file.hdf5', 'w')
+# data_file.create_dataset('model_weights', data=data_matrix)
+# data_file.create_dataset('optimizer_weights', data=data_matrix)
+#
+# data_file.close()
